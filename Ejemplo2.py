db1e20f8345e967fb913f34f57a002942efa16e5
Gsantiago31 <84323828+Gsantiago31@users.noreply.github.com>
2021-05-17 07:45:43 -0500

    Create Ejemplo2.py

---
+++ Ejemplo2.py
@@ -0,0 +1,14 @@
+# Inicio de condicional
+
+# Caso  : La sentencia 'if'
+# Ingresando la informacion 
+
+numero_1 = int(input('Escriba numero 1 :'))
+
+numero_2 = int(input('Escriba numero 2 :'))
+
+if numero_1 >= numero_2:
+  print ('el numero 1 es mayor')
+  
+else:
+  print('elnumero 2')
