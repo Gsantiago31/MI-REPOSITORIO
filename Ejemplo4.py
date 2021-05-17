4bb16d4f1ae44169c5be7c2419e8bc2ff5b7b3f7
Gsantiago31 <84323828+Gsantiago31@users.noreply.github.com>
2021-05-17 07:45:03 -0500

    Create Ejemplo4.py

---
+++ Ejemplo4.py
@@ -0,0 +1,15 @@
+
+# Inicio de condicional
+
+# Caso  : La sentencia 'if'
+# Caso : Multiples condiciones dentro de la misma linea del if
+
+numero = float(input('Escriba el numero : '))
+
+if numero >= 20  and numero <=40:
+    
+        print('el numero esta entre 20 y 40')
+
+   
+else :
+        print ('el numero NO esta entre 20 y 40')
