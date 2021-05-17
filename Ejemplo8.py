8c0fe9bc893da49cdf01ac86d712ef0705c92bcb
Gsantiago31 <84323828+Gsantiago31@users.noreply.github.com>
2021-05-17 09:04:46 -0500

    Create Ejemplo8.py

---
+++ Ejemplo8.py
@@ -0,0 +1,11 @@
+# Primer ejemplo ciclo 'for'
+
+numeros = [1,2,3,4,]
+
+total = 0
+for i in range(len(numeros)):
+      total = total + numeros[i]
+      print(total)
+
+      
+print('Total es : ',total)
